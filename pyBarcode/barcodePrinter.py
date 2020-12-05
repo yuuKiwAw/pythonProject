11d609ccecb478c8e8a84986e858df0c882e2df4
yuuKiwAw <zhangtuo1999@gmail.com>
2020-12-06 01:10:42 +0800

    interactive button

--- pyBarcode/barcodePrinter.py
+++ pyBarcode/barcodePrinter.py
@@ -1,30 +1,35 @@
 import barcode
 
+import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter.filedialog import *
 from barcode.writer import ImageWriter
 
 def main():
-    window = Tk()
+    window = tk.Tk()
     window_Width = window.winfo_screenwidth()
     window_Height = window.winfo_screenheight()
     window.title("barcode")
     ww = 400
-    wh = 180
+    wh = 400
     x = (window_Width-ww)/2
     y = (window_Height-wh)/2
     window.geometry("%dx%d+%d+%d" %(ww,wh,x,y))
     
-    
-    code_Number = input("enter the barcode number:")   #获取用户输入的值
-    
+   
     def toImage():      #转成png图片格式 
-        code_128 =  barcode.get('code128',code_Number,writer=ImageWriter())
+        var = code_Number_Label.get()
+        code_128 =  barcode.get('code128',var,writer=ImageWriter())
         file_name = code_128.save('code') 
-    
-    toImage()
 
+    ######################----GUI界面----######################
+    code_creater_Button = Button(window,width=10,height=1,text="Create",command=toImage)
+    code_creater_Button.place(relx=0.5,rely=0.2,anchor='center')
+    code_Number_Label = Entry(window,width=30)
+    code_Number_Label.place(relx=0.5,rely=0.1,anchor='center')
+    ######################----GUI界面----######################
+    
     window.mainloop()
 
 
